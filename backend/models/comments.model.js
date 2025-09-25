b7d7d343fa59ba801a4c40f84cd3e87dbdc35519
Divya19Gupta <divya2207.work@gmail.com>
2025-09-25 19:41:59 +0200

    added more apis to api.http

--- backend/models/comments.model.js
+++ backend/models/comments.model.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
 const commentsSchema = mongoose.Schema({
-     userId: {
+    userId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
-        },
+    },
     postId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
-        },
-        body: {
-            type: String,
-            required: true,
-        },
+    },
+    body: {
+        type: String,
+        required: true,
+    },
 });
 
 const Comment = mongoose.model('Comment', commentsSchema);
