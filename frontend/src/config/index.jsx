0fe3657e282d3ad0f4616c77e2fa80619480fa6c
Divya19Gupta <divya2207.work@gmail.com>
2025-10-03 21:18:51 +0200

    added link of the deployed backend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 const {default:axios} = require('axios');
 // export const BASE_URL = 'https://alumni-network-app.onrender.com/';
-export const BASE_URL = 'http://localhost:9090'
+export const BASE_URL = 'https://alumni-network-app-1.onrender.com/'
 export const clientServer = axios.create({
     baseURL: BASE_URL,
 })
